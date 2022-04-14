f5e6f93957a7d828b37761236a613521acc05c00
Joao-Gontijo <jvgontijo166@gmail.com>
2022-04-13 22:29:16 -0300

    serializando objeto cliente

---
+++ src/io/github/joaogontijo/java/io/teste/TesteSerializacaoCliente.java
@@ -0,0 +1,25 @@
+package io.github.joaogontijo.java.io.teste;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class TesteSerializacaoCliente {
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+//		Cliente cliente = new Cliente();
+//		cliente.setCpf("12334590");
+//		cliente.setNome("João");
+//		cliente.setProfissao("dev");
+//		
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+//		oos.writeObject(cliente);
+//		oos.close();
+		
+		ObjectInputStream ios = new ObjectInputStream(new FileInputStream("cliente.bin"));
+		Cliente cliente = (Cliente) ios.readObject();
+		System.out.println(cliente.getNome());
+	}
+}
